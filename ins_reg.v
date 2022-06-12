6288bb2bb4ef672b595aeca09d23422a9abdde20
date20220616 <107328793+date20220616@users.noreply.github.com>
2022-06-12 17:05:00 +0800

    Update ins_reg.v

--- ins_reg.v
+++ ins_reg.v
@@ -1,4 +1,3 @@
-// instruction register
 module ins_reg(data, fetch, clk, rst, ins, ad1, ad2);
 input clk, rst;
 input [1:0] fetch;
@@ -8,8 +7,8 @@ output [4:0] ad1;
 output [7:0] ad2;
 reg [7:0] ins_p1, ins_p2;
 reg [2:0] state;
-assign ins = ins_p1[7:5]; //high 3 bits, instructions
-assign ad1 = ins_p1[4:0]; //low 5 bits, register address
+  assign ins = ins_p1[7:5]; //高3位为指令编码
+  assign ad1 = ins_p1[4:0]; //低5位为通用寄存器地址
 assign ad2 = ins_p2;
 always @(posedge clk or negedge rst) begin
 if(!rst) begin
@@ -17,11 +16,11 @@ ins_p1 <= 8'd0;
 ins_p2 <= 8'd0;
 end
 else begin
-if(fetch==2'b01) begin //fetch==2'b01 operation1, to fetch data from REG
+  if(fetch==2'b01) begin //输入控制信号为01，表示当前数据总线上为指令，将其从ins和ad1输出
 ins_p1 <= data;
 ins_p2 <= ins_p2;
 end
-else if(fetch==2'b10) begin //fetch==2'b10 operation2, to fetch data
+  else if(fetch==2'b10) begin //输入控制信号为10，表示当前数据总线上为数据，将其从ad2输出到地址选择器
 ins_p1 <= ins_p1;
 ins_p2 <= data;
 end
