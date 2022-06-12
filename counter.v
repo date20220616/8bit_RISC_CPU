17942162527bc105d51729f4a610e5420bf0b04a
date20220616 <107328793+date20220616@users.noreply.github.com>
2022-06-12 16:22:24 +0800

    Update counter.v

--- counter.v
+++ counter.v
@@ -1,4 +1,3 @@
-//PC, program counter
 module counter(pc_addr, clock, rst, ena);
 input clock, rst, ena;
 output reg [7:0] pc_addr;
@@ -10,3 +9,4 @@ else pc_addr <= pc_addr;
 end
 end
 endmodule
+//异步清零。指令存储在rom中，因此每次pc_addr加1。
