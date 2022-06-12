b3634445c7f32534311c71551aae8db995ffeb41
date20220616 <107328793+date20220616@users.noreply.github.com>
2022-06-12 17:10:40 +0800

    Update ram.v

--- ram.v
+++ ram.v
@@ -5,9 +5,9 @@
  
    reg [7:0] ram[255:0];
  
-   assign data = (read&&ena)? ram[addr]:8'hzz;     // read data from RAM
- 
-   always @(posedge write) begin   // write data to RAM
+   assign data = (read&&ena)? ram[addr]:8'hzz;     
+   always @(posedge write) begin   
        ram[addr] <= data;
    end
    endmodule
+//读信号和使能信号ena有效时，输出对应地址存储数据，写信号有效时，输入和输出写入对应地址。
