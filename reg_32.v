c7309e7585f8ab6fed0490819281c1c387e1b23d
date20220616 <107328793+date20220616@users.noreply.github.com>
2022-06-12 17:13:23 +0800

    Update reg_32.v

--- reg_32.v
+++ reg_32.v
@@ -3,15 +3,12 @@ input write, read, clk;
 input [7:0] in;
 input [7:0] addr; 
 output [7:0] data;
-
-reg [7:0] R[31:0]; //32Byte
+reg [7:0] R[31:0]; 
 wire [4:0] r_addr;
-
 assign r_addr = addr[4:0];
-assign data = (read)? R[r_addr]:8'hzz;	//read enable
-
-always @(posedge clk) begin				//write, clk posedge
+assign data = (read)? R[r_addr]:8'hzz;
+always @(posedge clk) begin				
 	if(write)  R[r_addr] <= in; 
 end
 endmodule
-
+//写信号有效时，从alu输出数据输入存储器，存储到寄存及特定地址；读信号有效时候，寄存器数据输出到总线。
